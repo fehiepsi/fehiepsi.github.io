1e5562d7227270cf99b19c0ecc405dda8c771183
fehiepsi <fehiepsi@gmail.com>
2018-12-23 10:50:19 +0900

    add google analytics
    Nikola version: 8.0.1

--- conf.py
+++ conf.py
@@ -1235,7 +1235,17 @@ EXTRA_HEAD_DATA = """
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-# BODY_END = ""
+BODY_END = """
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131299125-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-131299125-1');
+</script>
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
